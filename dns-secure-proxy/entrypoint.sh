e4b9b8780adc58246ad92fe2e0d9bebf45e57388
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2020-04-14 23:48:54 -0600

    dns-secure-proxy: add support for hosts.d directory with dnsmasq

--- dns-secure-proxy/entrypoint.sh
+++ dns-secure-proxy/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-dnsmasq --no-daemon ${DNSMASQ_OPTS:-} &
+dnsmasq --no-daemon --addn-hosts /etc/hosts.d/ ${DNSMASQ_OPTS:-} &
 dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml ${DNSCRYPT_PROXY_OPTS:-} &
 
 wait -n
