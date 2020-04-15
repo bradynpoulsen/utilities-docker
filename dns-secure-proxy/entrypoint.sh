ae3ce90bd125d7974a404acf0a8a554018531e0d
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2020-04-15 00:22:14 -0600

    dns-secure-proxy: switch to use --hostsdir option dnsmasq

--- dns-secure-proxy/entrypoint.sh
+++ dns-secure-proxy/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-dnsmasq --no-daemon --addn-hosts /etc/hosts.d/ ${DNSMASQ_OPTS:-} &
+dnsmasq --no-daemon --hostsdir /etc/hosts.d ${DNSMASQ_OPTS:-} &
 dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml ${DNSCRYPT_PROXY_OPTS:-} &
 
 wait -n
